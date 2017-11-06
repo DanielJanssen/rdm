f3dec321d671f430a2e58090f28700db3250a798
danieljanssen@canda.com <rt57@DEN0251.canda.biz>
2017-11-06 08:40:18 +0100

    write method via elResolver and PropertyEditor

---
+++ rdm-web/src/main/java/de/th_koeln/rdm/elresolver/FluentElResolver.java
@@ -0,0 +1,62 @@
+package de.th_koeln.rdm.elresolver;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.el.BeanELResolver;
+import javax.el.ELContext;
+
+public class FluentElResolver extends BeanELResolver {
+
+	@Override
+	public boolean isReadOnly(ELContext context, Object base, Object property) {
+		if (base == null || !(property instanceof String)) {
+			return true;
+		}
+		Method method = getFluentMethod(context, base, property.toString());
+		if (method == null) {
+			return true;
+		}
+		context.setPropertyResolved(true);
+		return false;
+	}
+
+	@Override
+	public void setValue(ELContext context, Object base, Object property, Object value) {
+		if (base == null || !(property instanceof String)) {
+			return;
+		}
+		Method method = getFluentMethod(context, base, property.toString());
+		if (method == null) {
+			return;
+		}
+		try {
+			method.invoke(base, value);
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		} catch (InvocationTargetException e) {
+			if (e.getTargetException() instanceof RuntimeException) {
+				throw (RuntimeException) e.getTargetException();
+			} else {
+				throw new IllegalStateException(e.getTargetException());
+			}
+		}
+		context.setPropertyResolved(true);
+	}
+
+	private Method getFluentMethod(ELContext context, Object base, String name) {
+		for (Method method : base.getClass().getMethods()) {
+			if (isFluentWriteMethod(method, context, base, name)) {
+				return method;
+			}
+		}
+		return null;
+	}
+
+	private boolean isFluentWriteMethod(Method method, ELContext context, Object base, String property) {
+		String suffix = Character.toUpperCase(property.charAt(0)) + property.substring(1);
+		// TODO rt57, 03.11.2017: String.class oder auf den Typen zugreifen?
+		return method.getName().endsWith(suffix) && method.getParameterTypes().length == 1 && method.getParameterTypes()[0] == getType(context, base, property);
+	}
+
+}
---
+++ rdm-web/src/main/java/de/th_koeln/rdm/propertyeditor/RdmPropertyEditor.java
@@ -0,0 +1,13 @@
+package de.th_koeln.rdm.propertyeditor;
+
+import java.beans.PropertyEditorSupport;
+
+//https://howtodoinjava.com/spring/spring-boot/custom-property-editor-example/
+//https://www.buschmais.de/2009/08/05/initialisierung-von-jsf-managed-beans/
+public class RdmPropertyEditor extends PropertyEditorSupport {
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+
+	}
+
+}
